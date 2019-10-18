e4d6bc7d724924fa856437e8897a2ddc1acfe5a7
kat-star <katherinewkwong@gmail.com>
2019-10-17 18:06:17 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+  
+  def index
+    @dogs = Dog.get_employees(params[:sort])
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+  before_action :find_employee, only: [:show, :edit, :update ]
+  
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+ 
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create  
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path
+    end
+  end 
+  
+  def edit
+    @dogs = Dog.all
+  end
+
+  def update
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
+  def find_employee
+    @employee = Employee.find(params[:id])
+  end
+
 end
